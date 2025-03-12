7920292fd7cc86ffb4c52b9082d930e0028dbbdf
Justin Hooper <jhooperonline@gmail.com>
2025-03-12 08:24:57 -0600

    feat:  Initial commit, working chatbot.

---
+++ README.md
@@ -0,0 +1,14 @@
+## The Oracle
+A simple chatbot designed to interface with a desired Ollama hosted local LLM model.
+
+## Usage
+Eventually the Oracle will be designed to run as a self-hosted docker image providing
+a remote API service on which a web-based interface can be accessed.  For now, simply
+run bot_base.py as the main file:
+    python oracle.py
+
+A basic chat loop is implemented.  You can enter 'goodbye' to quit.
+
+## Changelog
+- 0.1.0: Initial release
+
---
+++ oracle.py
@@ -0,0 +1,149 @@
+import typing as t
+import typing_extensions as te
+import os
+import dotenv as de
+import langgraph.graph as lg
+import langgraph.graph.message as lgm
+import langchain_ollama as lo
+import textwrap as tw
+
+
+def model_factory(
+    *, model_interface, model_name, api_key, temperature=0.5, endpoint=None, **kwargs
+):
+    print(f"Model Interface Requested: {model_interface}")
+    if os.getenv("MATURITY") == "test":
+        # Return a mock interface here eventually for testing
+        return None
+    if model_interface == "ChatOllama":
+        if os.getenv("OLLAMA_ENDPOINT") and endpoint is None:
+            endpoint = os.getenv("OLLAMA_ENDPOINT")
+            if "http://" not in endpoint:
+                endpoint = "http://" + endpoint
+        else:
+            endpoint = "http://localhost:11434"
+        if api_key is None:
+            api_key = "Ollama_No_API_Key"
+        try:
+            llm = lo.ChatOllama(
+                model=model_name,
+                endpoint=endpoint,
+                api_key=api_key,
+                temperature=temperature,
+                **kwargs,
+            )
+            return llm
+        except Exception as err:
+            print(f"Error instantiating the ChatOllama interface: {err}")
+    else:
+        print(f"Models other than ChatOllama are not currently implemented, sorry.")
+        return None
+
+
+class State(te.TypedDict):
+    messages: t.Annotated[list, lgm.add_messages]
+
+
+class generic_chat_node:
+    _self = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._self is None:
+            cls._self = super().__new__(cls)
+            cls._self._initialize(*args, **kwargs)
+        return cls._self
+
+    def _initialize(
+        self,
+        interface: str,
+        model: str,
+        api_key: str,
+        endpoint: str,
+        temperature=0.5,
+        **kwargs,
+    ):
+        self.model_name = model
+        self.model_interface = interface
+        self.endpoint = endpoint
+        self.temperature = temperature
+        self.llm = model_factory(
+            model_interface=interface,
+            model_name=self.model_name,
+            api_key=api_key,
+            temperature=temperature,
+            endpoint=self.endpoint,
+            **kwargs,
+        )
+
+    def get_model_name(self):
+        """
+        Return the name of the model.
+
+        Returns:
+            str: The name of the model.
+        """
+        return self.model_name
+
+    def get_model_type(self):
+        """
+        Return the Langchain interface class of the model.
+
+        Returns:
+            str: The Langchain interface class of the model.
+        """
+        return self.model_interface
+
+    def get_response(self, state: State):
+        return {"messages": self.llm.invoke(state["messages"])}
+
+
+if __name__ == "__main__":
+    print("Welcome to the base of the chatbot.")
+
+    de.load_dotenv()
+    graph_builder = lg.StateGraph(State)
+    chatnode = generic_chat_node(
+        interface="ChatOllama",
+        model="mistral_nemo_conservative",
+        api_key=None,
+        endpoint=None,
+        temperature=0.5,
+        num_predict=1500,
+    )
+    graph_builder.add_node("chatnode", chatnode.get_response)
+    graph_builder.add_edge(lg.START, "chatnode")
+    graph_builder.add_edge("chatnode", lg.END)
+    graph = graph_builder.compile()
+
+    def stream_graph_updates(user_input: str):
+        for event in graph.stream(
+            {"messages": [{"role": "user", "content": user_input}]}
+        ):
+            {"messages": [{"role": "user", "content": user_input}]}
+            for value in event.values():
+                print("\n[Assistant]")
+                print(
+                    tw.fill(
+                        value["messages"].content,
+                        width=120,
+                        initial_indent="    ",
+                        subsequent_indent="        ",
+                        replace_whitespace=False,
+                    )
+                )
+
+    while True:
+        try:
+            user_input = input(f"\n[User]  ")
+            if user_input.lower() in ["quit", "exit", "q", "bye", "goodbye"]:
+                print("Goodbye!")
+                break
+            stream_graph_updates(user_input)
+        except:
+            # fallback if input() is not available
+            user_input = (
+                "How much wood could a woodchuck chuck if a woodchuck had a chainsaw?"
+            )
+            print("User: " + user_input)
+            stream_graph_updates(user_input)
+            break
